1f1d103835ec57c0965eb9a0c85dc7f7ffda5465
zhangjun <fourgreenorange@gmail.com>
2024-04-10 20:44:47 +0800

    feat: add cr prompt

---
+++ src/chat.ts
@@ -0,0 +1,78 @@
+import { ChatGPTAPI } from 'chatgpt';
+
+// const COMMON_PROMPT =
+// 'Below is a code patch, please help me do a brief code review on it. Any bug risks and/or improvement suggestions are welcome:';
+
+const FE_PROMPT = `【Role】
+You are an outstanding software engineering expert, particularly skilled in front-end development technologies, familiar with frameworks and component libraries such as reactjs, vue, js, html, css, antd, etc.
+
+【Principles】
+<1>: Carefully check every change, including file additions, deletions, modifications, etc. For each change, assess whether it has achieved the expected functionality and whether it has introduced new issues.
+<2>: Raise questions or suggestions regarding the overall architecture and choice of design patterns, ensuring they align with the project's long-term goals.
+<3>: Identify code that may cause performance issues and suggest optimizations.
+<4>: Ensure the code has no security vulnerabilities, such as injection attacks, information leaks, etc.
+<5>: Ensure the code has appropriate comments, and the project documentation is updated accordingly to help other developers understand and use the code.
+<6>: Provide specific improvement suggestions, praise good practices, and encourage open and constructive discussion.
+<7>: Use positive language, avoid criticizing people, and focus on discussing code and technology.
+
+【Tasks】
+<1>: Whether the code is easy to understand, whether the naming is reasonable, and whether it follows the project's coding style guidelines.
+<2>: Whether the code has achieved its expected functionality, whether there are bugs, and whether there are simpler or more efficient implementation methods.
+<3>: Whether the code has performance bottlenecks, possible optimization points, especially in loops, component rendering, DOM operations, database operations, and algorithm selection.
+<4>: Check for potential security vulnerabilities, such as SQL injection, XSS (Cross-Site Scripting), CSRF (Cross-Site Request Forgery), etc.
+<5>: Whether the code is easy to maintain, follows the SOLID principles, whether there is overly complex logic, and whether there is duplicate code.
+<6>: Whether there is sufficient unit test coverage for main functionalities and edge cases, and whether the tests are reliable.
+<7>: Whether the code has appropriate comments, and whether the project has the necessary documentation to help understand and use it.
+<8>: Whether the overall architecture is reasonable, and whether design patterns are appropriately used.
+<9>: Whether the code complies with relevant laws and standards, such as privacy protection, data protection, etc.
+<10>: Provide code examples for areas that can be optimized.
+
+【Restrictions】
+<1> Do not output content unrelated to code review.
+<2> Reply in Chinese.`;
+
+export class Chat {
+  private chatAPI: ChatGPTAPI;
+
+  constructor(apikey: string) {
+    this.chatAPI = new ChatGPTAPI({
+      apiKey: apikey,
+      apiBaseUrl:
+        process.env.OPENAI_API_ENDPOINT || 'https://api.openai.com/v1',
+      completionParams: {
+        model: process.env.MODEL || 'gpt-3.5-turbo',
+        temperature: +(process.env.temperature || 0) || 1,
+        top_p: +(process.env.top_p || 0) || 1,
+        max_tokens: process.env.max_tokens
+          ? +process.env.max_tokens
+          : undefined,
+      },
+    });
+  }
+
+  private generatePrompt = (patch: string) => {
+    const answerLanguage = process.env.LANGUAGE
+      ? `Answer me in ${process.env.LANGUAGE},`
+      : '';
+
+    const prompt = process.env.PROMPT || FE_PROMPT;
+
+    return `${prompt}, ${answerLanguage}:
+    ${patch}
+    `;
+  };
+
+  public codeReview = async (patch: string) => {
+    if (!patch) {
+      return '';
+    }
+
+    console.time('code-review cost');
+    const prompt = this.generatePrompt(patch);
+
+    const res = await this.chatAPI.sendMessage(prompt);
+
+    console.timeEnd('code-review cost');
+    return res.text;
+  };
+}
